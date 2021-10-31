ad10e9814db51d4ac2c70f35583cd608ad634b17
mufaddal2153 <mufaddalhatim53@gmail.com>
2021-10-31 16:42:43 +0500

    second commit

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def index():
         query = request.form["search"]
         query = query.replace(' ', '+')
         results = query_search(query)
+        # final_arr = []
+        # while True:
+            # final_arr += results
+            # if (len(list) > 100):
+            #     break
         return render_template('index.html', arr=results)
     return render_template('index.html')
 
@@ -17,19 +22,22 @@ def api():
     query = request.args.get('search')
     count = request.args.get('count')
     query = query.replace(' ', '+')
-    results=query_search(query)
-
+    # final_arr = []
+    # while True:
+    results = query_search(query)
+    # final_arr += results
+    # if (len(list) > 100):
+    #     break
     result_json = json.dumps(results)
-
     return result_json
 
 def query_search(query):
-    counter = 0
     results = []
     URL = f"https://shopping.google.com/search?q={query}"
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
     headers = {"user-agent": USER_AGENT}
     resp = requests.get(URL, headers=headers)
+    print(resp)
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.content, "html.parser")
         for g in soup.find_all('article'):
@@ -39,7 +47,6 @@ def query_search(query):
             prices = anchors[0].find('div').find('span').text
             title = arr[4].find('div').text
             img = arr[2].find('img')['src']
-            print(img)
             item = {
                 "prices": prices,
                 "link": link,
